83b0f6e0603fba6acb1bacebeade44be233c718b
wogal <egilgeous@gmail.com>
2018-11-21 19:37:51 -0500

    ver 100

--- app/src/main/java/ca/wogal/food_select_100/MainActivity.kt
+++ app/src/main/java/ca/wogal/food_select_100/MainActivity.kt
@@ -2,15 +2,18 @@ package ca.wogal.food_select_100
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Example of a call to a native method
-   //     sample_text.text = stringFromJNI()
+        sample_text.text = " wogal ";
+        sample_text.text = stringFromJNI()
     }
 
     /**
